5e80c6f966642a6f0055a60e2129742a42b3ffdd
ninzagamerz <106005123+ninzagamerz@users.noreply.github.com>
2024-09-12 08:58:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,28 +4,28 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = os.environ.get("API_ID", "28776072")
+    API_HASH = os.environ.get("API_HASH", "b3a786dce1f4e7d56674b7cadfde3c9d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7037453058:AAGDtxFckOjQryf7pwPVK7zdiMSW-vL6w6s")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002471632904")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002392831227")
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
-    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
-    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
-    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
-    MONGODB_URI = os.environ.get("MONGODB_URI")
+    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 30))
+    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "0a578760a5a837896f60")
+    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "2qJoVG3wLqSZwkr")
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://ftm:ftm@ftmbot.z5iox.mongodb.net/?retryWrites=true&w=majority&appName=ftmbot")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "7042535787"))
 
     START_TEXT = """
-Hi Unkil, I am Video Merge Bot!
+Hi {name}, I am Video Merge Bot!
 I can Merge Multiple Videos in One Video. Video Formats should be same.
 
-Made by @AbirHasan2005
+Made with by @ftmdeveloper
 """
-    CAPTION = "Video Merged by @{}\n\nMade by @AbirHasan2005"
+    CAPTION = "Video Merged by @{}\n\n Made with 💕 by @ftmdeveloper"
     PROGRESS = """
 Percentage : {0}%
 Done: {1}
